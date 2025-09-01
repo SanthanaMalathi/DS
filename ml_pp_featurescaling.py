0417ef5352dfa68e3cfca23b296160f5d966fca5
SanthanaMalathi <malathi.eswaran1991@gamil.com>
2025-09-01 14:45:40 +0530

    changes added

--- ml_pp_featurescaling.py
+++ ml_pp_featurescaling.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 df=pd.read_csv("diamonds.csv")
 
 standard_scaler_object= StandardScaler()
 df["converted_price"]=standard_scaler_object.fit_transform(df[['price']])
+print(df[["price","converted_price"]])
+
+minmax_scaler= MinMaxScaler()
+df["converted_price"]=minmax_scaler.fit_transform(df[['price']])
 print(df[["price","converted_price"]])
 
 
